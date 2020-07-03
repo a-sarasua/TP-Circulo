143554aedc1e604ccb762d714af4f831db9da9cd
Agustin Sarasua <64442857+a-sarasua@users.noreply.github.com>
2020-07-02 22:57:44 -0300

    Movimiento aleatorio terminado
    Se terminaron detalles para hacer más cómodo el movimiento aleatorio

--- TP-Circulo/Script/main.js
+++ TP-Circulo/Script/main.js
@@ -10,7 +10,8 @@ window.onload = function() {
 	const greenButton = document.querySelector('.greenButton')
 	const redButton = document.querySelector('.redButton')
 
-	const randomButton = document.querySelector('.randomButton')
+	const playButton = document.querySelector('.playButton')
+	const pauseButton = document.querySelector('.pauseButton')
 	const resetButton = document.querySelector('.resetButton')
 
 	const circle = document.querySelector('.circle')
@@ -18,6 +19,8 @@ window.onload = function() {
 	const initialTop = circle.offsetTop
 	const initialLeft = circle.offsetLeft
 	let mousedownID = -1
+	let randomOff = true
+	let playerPressed = false
 
 	function wait(ms){
 	   const start = new Date().getTime()
@@ -29,61 +32,53 @@ window.onload = function() {
 
 	function moveLeft() {
 		const newPosition = circle.offsetLeft - MOVEMENT
-		if (newPosition < -2) {
-			return
-		} else {
-			circle.style.left = newPosition + 'px' 
-		}
+		if (newPosition < -2) return
+		circle.style.left = newPosition + 'px' 
 	}
 
 	function moveUp() {
 		const newPosition = circle.offsetTop - MOVEMENT 
-		if (newPosition < -2) {
-			return
-		} else {
-			circle.style.top = newPosition + 'px'
-		}
+		if (newPosition < -2) return
+		circle.style.top = newPosition + 'px'
 	}
 
 	function moveDown() {
 		const newPosition = circle.offsetTop + MOVEMENT
-		if (initialTop*2 < newPosition){
-			return
-		} else {
-			circle.style.top = newPosition + 'px'
-		}
+		if (initialTop*2 < newPosition) return
+		circle.style.top = newPosition + 'px'
 	}
 
 	function moveRight() {
 		const newPosition = circle.offsetLeft + 25
-		if (initialLeft*2 < newPosition) {
-			return
-		} else {
-			circle.style.left = newPosition + 'px'		
-		}
+		if (initialLeft*2 < newPosition) return
+		circle.style.left = newPosition + 'px'		
 	}
 
 	function chooseMovement(key) {
-		wait(20)
-		switch (key) {
-			case 37:
-				moveLeft()
-				break
-			case 38:
-				moveUp()
-				break
-			case 39:
-				moveRight()
-				break
-			case 40:
-				moveDown()
-				break				
-			default:
-				return
-		}	
+		if(playerPressed && !randomOff){
+			return
+		} else {
+			wait(20)
+			switch (key) {
+				case 37:
+					moveLeft()
+					break
+				case 38:
+					moveUp()
+					break
+				case 39:
+					moveRight()
+					break
+				case 40:
+					moveDown()
+					break				
+				default:
+					return
+			}		
+		}
 	}
 
-	function mouseDownInterval(number){
+	function movementInterval(number){
 		if(mousedownID === -1) {
 			mousedownID = setInterval(function(){
 				chooseMovement(number)
@@ -91,8 +86,9 @@ window.onload = function() {
 		}
 	}
 
-	function mouseUpinterruption() {
-		if(mousedownID !== -1) {
+	function movementInterrupt() {
+		console.log(randomOff)
+		if(mousedownID !== -1 && randomOff) {
 			clearInterval(mousedownID)
 			mousedownID = -1
 		}
@@ -102,15 +98,20 @@ window.onload = function() {
 	greenButton.addEventListener('click', function(){circle.style.backgroundColor = '#00FF00'})
 	redButton.addEventListener('click', function(){circle.style.backgroundColor = '#FF0000'})
 
-	document.addEventListener('keydown', function(){chooseMovement(event.keyCode)})
+	document.addEventListener('keydown', function(){
+		playerPressed = true
+		chooseMovement(event.keyCode)
+		playerPressed = false
+	})
 
-	leftButton.addEventListener('mousedown', function(){mouseDownInterval(37)})
-	upButton.addEventListener('mousedown', function(){mouseDownInterval(38)})
-	rightButton.addEventListener('mousedown', function(){mouseDownInterval(39)})
-	downButton.addEventListener('mousedown', function(){mouseDownInterval(40)})
-	document.addEventListener('mouseup', mouseUpinterruption)
-	
-	randomButton.addEventListener('mousedown', function(){
+	leftButton.addEventListener('mousedown', function(){movementInterval(37)})
+	upButton.addEventListener('mousedown', function(){movementInterval(38)})
+	rightButton.addEventListener('mousedown', function(){movementInterval(39)})
+	downButton.addEventListener('mousedown', function(){movementInterval(40)})
+	document.addEventListener('mouseup', movementInterrupt)
+
+	playButton.addEventListener('click', function(){
+		randomOff = false
 		if(mousedownID === -1) {
 			mousedownID = setInterval(function(){
 				const randomDirection = Math.ceil(Math.random() * (40 - 36) + 36)
@@ -118,15 +119,19 @@ window.onload = function() {
 			}, 50)
 		}
 	})
-	randomButton.addEventListener('mouseup', mouseUpinterruption)
+	pauseButton.addEventListener('click', function(){
+		randomOff = true
+		movementInterrupt()
+	})
+
 	resetButton.addEventListener('click', function(){
+		if(!randomOff) {
+			randomOff = true
+			movementInterrupt()
+		}
 		circle.style.left = initialLeft + 'px'
 		circle.style.top = initialTop + 'px'
 	})
 
 }
 	
-
-
-
-
